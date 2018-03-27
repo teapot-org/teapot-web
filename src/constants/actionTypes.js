28ea33625e814158568a6bb8efcb9db8c21a7ae5
kolya-t <toporkov.kolya96@gmail.com>
2018-03-27 23:09:17 +0300

    add user profile page

--- src/constants/actionTypes.js
+++ src/constants/actionTypes.js
@@ -7,3 +7,11 @@ export const signInRequest = () => ({type: SIGN_IN_REQUEST});
 export const signInSuccess = (user) => ({type: SIGN_IN_SUCCESS, payload: user});
 export const signInFailure = () => ({type: SIGN_IN_FAILURE});
 export const signOut = () => ({type: SIGN_OUT});
+
+export const GET_USER_REQUEST = 'GET_USER_REQUEST';
+export const GET_USER_SUCCESS = 'GET_USER_SUCCESS';
+export const GET_USER_FAILURE = 'GET_USER_FAILURE';
+
+export const getUserRequest = () => ({type: GET_USER_REQUEST});
+export const getUserSuccess = (user) => ({type: GET_USER_SUCCESS, payload: user});
+export const getUserFailure = () => ({type: GET_USER_FAILURE});
--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,8 +1,10 @@
 import {combineReducers} from 'redux'
 import {routerReducer as router} from 'react-router-redux'
 import oauth from './oauth'
+import users from './users'
 
 export default combineReducers({
   router,
-  oauth
+  oauth,
+  users
 });
