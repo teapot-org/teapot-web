e1212053ee5ab15fe5dd24e4897705cae7523e8f
Barbosser <sar243102@gmail.com>
2018-03-31 11:21:48 +0300

    sign-up

--- src/components/widgets/Router/Router.js
+++ src/components/widgets/Router/Router.js
@@ -3,6 +3,7 @@ import {Route, Switch} from "react-router";
 import AuthorizedRoute from "../../widgets/AuthorizedRoute";
 import HomePage from "../../pages/HomePage";
 import SignInPage from "../../pages/SignInPage";
+import SignUpPage from "../../pages/SignUpPage";
 import Error404Page from "../../pages/Error404Page";
 
 class Router extends React.Component {
@@ -12,6 +13,7 @@ class Router extends React.Component {
         <Switch>
           <AuthorizedRoute path="/" exact component={HomePage}/>
           <Route path="/sign-in" exact component={SignInPage}/>
+          <Route path="/sign-up" exact component={SignUpPage}/>
           <Route component={Error404Page}/>
         </Switch>
       </main>
