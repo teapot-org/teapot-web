1bf6d39b069b933a1e88efe83f08e0708befb605
kolya-t <toporkov.kolya96@gmail.com>
2018-04-04 13:47:00 +0300

    styled sign up form

--- src/components/pages/SignUpPage/SignUpPage.js
+++ src/components/pages/SignUpPage/SignUpPage.js
@@ -1,12 +1,22 @@
 import React from 'react'
+import {Grid, Segment} from 'semantic-ui-react'
+
 import SignUpForm from "../../widgets/SignUpForm";
 
 class SignUpPage extends React.Component {
   render() {
     return (
-      <div>
-        <SignUpForm/>
-      </div>
+      <Grid columns='equal'>
+        <Grid.Row>
+          <Grid.Column/>
+          <Grid.Column mobile={16} tablet={8} computer={6}>
+            <Segment padded color='blue'>
+              <SignUpForm/>
+            </Segment>
+          </Grid.Column>
+          <Grid.Column/>
+        </Grid.Row>
+      </Grid>
     );
   }
 }
