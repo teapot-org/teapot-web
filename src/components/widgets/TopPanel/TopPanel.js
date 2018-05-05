626303c0227bf7f7a6a300e6f662ec001104f937
kolya-t <toporkov.kolya96@gmail.com>
2018-05-05 14:49:27 +0300

    show different top panels fro authorized users and not

--- src/components/widgets/TopPanel/TopPanel.js
+++ src/components/widgets/TopPanel/TopPanel.js
@@ -1,63 +1,35 @@
 import React from 'react'
 import {connect} from 'react-redux'
+import {Link} from 'react-router-dom'
 
 import {signOut} from '../../../constants/actionTypes'
 
 class TopPanel extends React.Component {
   render() {
-    // const {isAuthenticated, profile} = this.props.oauth;
-    // return (
-    //   <Menu borderless>
-    //     {isAuthenticated ? (
-    //       <Menu.Menu position='right'>
-    //         <Dropdown item simple text={profile.name}>
-    //           <Dropdown.Menu>
-    //             <Dropdown.Item
-    //               as={Link}
-    //               to='/'
-    //               onClick={this.props.signOut}
-    //             >
-    //               <Icon name='sign out'/>
-    //               Sign out
-    //             </Dropdown.Item>
-    //           </Dropdown.Menu>
-    //         </Dropdown>
-    //       </Menu.Menu>
-    //     ) : (
-    //       <Menu.Menu position='right'>
-    //         <Menu.Item as={Link} to='/sign-in'>
-    //           <Icon name='sign in'/>
-    //           Sign in
-    //         </Menu.Item>
-    //         <Menu.Item as={Link} to='/sign-up'>
-    //           <Icon name='add user'/>
-    //           Sign up
-    //         </Menu.Item>
-    //       </Menu.Menu>
-    //     )}
-    //   </Menu>
-    // );
-
+    const {isAuthenticated, profile} = this.props.oauth;
 
     return (
       <header>
         <div id="logo">Teapot</div>
-        <a href="#" className="button" id="organizations">Организации</a>
-        <a href="#" className="button" id="kanbans">Канбаны</a>
-        <ul id="username" className="button">
-          <a href="#">Username</a>
-          <ul className="submenu">
-            <li>
-              <a href="#">Профиль</a>
-            </li>
-            <li>
-              <a href="#">Настройки</a>
-            </li>
-            <li>
-              <a href="#">Выход</a>
-            </li>
-          </ul>
-        </ul>
+        {isAuthenticated ? (
+          <React.Fragment>
+            <a href="#" className="button" id="organizations">Организации</a>
+            <a href="#" className="button" id="kanbans">Канбаны</a>
+            <ul id="username" className="button">
+              <a>{profile.name}</a>
+              <ul className="submenu">
+                <li><Link to='/profile'>Профиль</Link></li>
+                {/*<li><a>Настройки</a></li>*/}
+                <li><a onClick={this.props.signOut}>Выход</a></li>
+              </ul>
+            </ul>
+          </React.Fragment>
+        ) : (
+          <React.Fragment>
+            <Link className="button" to='/sign-up'>Регистрация</Link>
+            <Link className="button" to='/sign-in'>Вход</Link>
+          </React.Fragment>
+        )}
       </header>
     );
   }
